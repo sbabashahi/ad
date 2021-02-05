7015ad715ebf991110bf406841f52b19f931a9b1
saeed <sbabashahi@gmail.com>
2021-02-05 13:00:26 +0330

    Add related db path

--- setting/database.py
+++ setting/database.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 def initialize_db(app):
     global db
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     db = SQLAlchemy(app)
     return db
 
